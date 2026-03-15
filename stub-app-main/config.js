d3519234595076b4ccc1ca5d7f38697f6547acc1
Emine Türk <emineturk3203@gmail.com>
2026-03-15 18:20:29 +0300

    Update config.js

--- stub-app-main/config.js
+++ stub-app-main/config.js
@@ -12,14 +12,12 @@ const BUTTON_LABELS = [
   "Button 2",
   "Location Search",
   "Button 4",
-  "Button 5",
+  "🥳🤓🤯",
   "Button 6",
   "Button 7",
-<<<<<<< HEAD
   "Button 8",
 
 
-=======
+
   "Random Dogs",
->>>>>>> 8853dc09b96bbf18801c89decc1df6cffacbf895
 ];
